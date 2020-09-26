0f6cb03ddfc513b48995b51c06a4a201e97c0611
elliottwuTW <elliott10009@gmail.com>
2020-09-26 21:43:14 +0800

    Initial commit - version 1.0.0

---
+++ README.md
@@ -0,0 +1,47 @@
+## My Express Restaurant List
+A movie list app with the given [restaurant.json](https://drive.google.com/open?id=1W-BD9-c8zJRYCwAD8yhqQdLwcUdN8GZi) using Node.js, Express, ExpressHandlebars, Bootstrap and other packages.
+
+
+### Features
+- The whole restaurants list shown on the main page with the corresponding information, including
+  - restaurant picture
+  - shop name
+  - category
+  - restaurant rating
+- Independent pages for each restaurant item with more information
+- Find your favorite restaurant by searching its name
+
+
+### Quick Start
+```
+# Create a project folder to start
+mkdir <project-folder>
+cd <project-folder>
+
+# Clone the project
+git clone https://github.com/elliottwuTW/express-restaurant-list.git
+
+# Install all dependencies
+npm install
+
+# Run the Express server
+npm run start
+
+# Or run the server with nodemon
+npm run dev
+
+# As the server starts successfully, the terminal will show "The server is running on http://localhost:3000".
+```
+
+### Package Versions
+- Node.js : 14.4.0
+- express : 4.17.1
+- express-handlebars : 5.1.0
+
+
+### App Info
+#### Author
+Elliott Wu [elliottwuTW](https://github.com/elliottwuTW)
+
+#### Version
+1.0.0
---
+++ app.js
@@ -0,0 +1,40 @@
+// Include the modules and files needed in Node.js
+const express = require('express')
+const exphdbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+
+// Create the Express app
+const app = express()
+const port = 3000
+
+// Set the template engine
+app.engine('handlebars', exphdbs({ defaultLayout: 'main' }))  // main.handlebars is the Layout
+app.set('view engine', 'handlebars')
+
+// Set the static files routing
+app.use(express.static('public'))
+
+// Set the main page routing
+app.get('/', (req, res) => {
+  res.render('index', { restaurantInfo: restaurantList.results })
+})
+
+// Set the restaurant page routing
+app.get('/restaurants/:id', (req, res) => {
+  const targetRestaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.id)
+  res.render('show', { restaurant: targetRestaurant })
+})
+
+// Set the restaurant-search routing
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const matchRestaurant = restaurantList.results.filter(restaurant => {
+    return (restaurant.name.toLowerCase().includes(keyword.toLowerCase())) || (restaurant.category.includes(keyword))
+  })
+  res.render('index', { restaurantInfo: matchRestaurant, keyword })
+})
+
+// Start and listen to the server
+app.listen(port, () => {
+  console.log(`The server is running on http://localhost:${port}.`)
+})
